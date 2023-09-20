9e79520da9b2e471f606ddd67e8fa35322629024
Okko <142894633+eoovve@users.noreply.github.com>
2023-09-20 22:47:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 NEZHA_SERVER=${NEZHA_SERVER:-'nz.f4i.cn'}     #哪吒面板地址
 NEZHA_PORT=${NEZHA_PORT:-'5555'}              #哪吒面板端口
-NEZHA_KEY=${NEZHA_KEY:-'Pr0xBHTAJvPgQvgZGD'}  #哪吒面板密钥
+NEZHA_KEY=${NEZHA_KEY:-'yThJkuXVZRPibFkhaD'}  #哪吒面板密钥
 NEZHA_TLS=${NEZHA_TLS:-''}               #哪吒面板是否开启tls，留空即为不开启
 ARGO_DOMAIN=${ARGO_DOMAIN:-''}           #ARGO隧道域名，留空为启用临时隧道
 ARGO_AUTH=${ARGO_AUTH:-''}           #ARGO隧道token或json，留空为启用临时隧道
